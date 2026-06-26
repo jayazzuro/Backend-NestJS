dac4042de545e7979afab4d291df1c955c6ddd04
jayazzuro <vogiahao2005@gmail.com>
2026-06-26 14:38:09 +0700

    Khoi tao nestjs va cau hinh db

---
+++ server/src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import databaseConfig from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+
+        host: configService.get('database.host'),
+
+        port: configService.get('database.port'),
+
+        username: configService.get('database.username'),
+
+        password: configService.get('database.password'),
+
+        database: configService.get('database.database'),
+
+        autoLoadEntities: true,
+
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class AppModule { }
